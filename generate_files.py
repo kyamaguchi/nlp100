a8d11ad0088c4005f9379bdd3db9ec260f80a0e6
kyamaguchi <kzh.yap@gmail.com>
2019-05-16 22:45:53 +0900

    Add script to generate files of questions

---
+++ generate_files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+import requests, bs4
+
+url = "http://www.cl.ecei.tohoku.ac.jp/nlp100/"
+base_dir = "python"
+
+res = requests.get(url)
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.content, "html.parser")
+
+sections = soup.find_all("h3")
+for section in sections:
+    if section.a is not None:
+        question = section.contents[1]
+        instruction = section.next_sibling.next_sibling.p.text
+        number = question.split(".")[0]
+        filepath = os.path.join(base_dir, number + '.py')
+
+        if os.path.exists(filepath):
+            print(filepath + " already exists.")
+        else:
+            print("Generating " + filepath)
+            with open(filepath, 'w') as f:
+                f.write("#!/usr/bin/env python\n")
+                f.write("\n")
+                f.write("print(\"" + question.replace('"', '\\"') + "\")\n")
+                f.write("print(\"" + instruction.replace('"', '\\"') + "\")\n")
