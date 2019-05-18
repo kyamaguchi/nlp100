5b44580d3f4ff12bc597c6f108a12f3f16e206d4
kyamaguchi <kzh.yap@gmail.com>
2019-05-18 23:26:08 +0900

    Solve NLP 06-07

--- python/06.py
+++ python/06.py
@@ -2,3 +2,22 @@
 
 print("06. 集合")
 print("\"paraparaparadise\"と\"paragraph\"に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，XとYの和集合，積集合，差集合を求めよ．さらに，'se'というbi-gramがXおよびYに含まれるかどうかを調べよ．")
+
+def ngram(n, lst):
+    result = []
+    for i in range(len(lst) - n + 1):
+        result.append(lst[i:i+n])
+    return result
+
+X = set(ngram(2, "paraparaparadise"))
+Y = set(ngram(2, "paragraph"))
+
+print("X: " + str(X))
+print("Y: " + str(Y))
+
+print("和集合: " + str(X | Y))
+print("積集合: " + str(X & Y))
+print("差集合: " + str(X - Y))
+
+print("'se' in X ? " + str('se' in X))
+print("'se' in Y ? " + str('se' in Y))
--- python/07.py
+++ python/07.py
@@ -2,3 +2,14 @@
 
 print("07. テンプレートによる文生成")
 print("引数x, y, zを受け取り「x時のyはz」という文字列を返す関数を実装せよ．さらに，x=12, y=\"気温\", z=22.4として，実行結果を確認せよ．")
+
+def xyz(x, y, z):
+    return str(x) + "時の" + str(y) + "は" + str(z)
+
+print(xyz(12, "気温", 22.4))
+
+
+def format_string(x, y, z):
+    return '{hour}時の{target}は{value}'.format(hour=x, target=y, value=z)
+
+print(format_string(12, '気温', 22.4))
