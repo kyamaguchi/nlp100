973865025d150788ce010c2708c414abe5959183
kyamaguchi <kzh.yap@gmail.com>
2019-05-20 23:18:10 +0900

    Solve NLP 11-13

--- python/11.py
+++ python/11.py
@@ -2,3 +2,9 @@
 
 print("11. タブをスペースに置換")
 print("タブ1文字につきスペース1文字に置換せよ．確認にはsedコマンド，trコマンド，もしくはexpandコマンドを用いよ．")
+
+print(open('hightemp.txt').read().replace("\t", " "))
+
+import subprocess
+res = subprocess.check_output("cat hightemp.txt | tr '\t' ' ' ", shell=True, universal_newlines=True)
+print(res)
--- python/13.py
+++ python/13.py
@@ -2,3 +2,16 @@
 
 print("13. col1.txtとcol2.txtをマージ")
 print("12で作ったcol1.txtとcol2.txtを結合し，元のファイルの1列目と2列目をタブ区切りで並べたテキストファイルを作成せよ．確認にはpasteコマンドを用いよ．")
+
+fname = 'hightemp.txt'
+with open('cols.txt', mode='w') as cols_file, \
+        open('col1.txt') as col1_file, \
+        open('col2.txt') as col2_file:
+    for line in zip(col1_file.readlines(), col2_file.readlines()):
+        cols_file.write('\t'.join([x.rstrip() for x in line]) + '\n')
+
+print(open('cols.txt').read())
+
+import subprocess
+res = subprocess.check_output("paste col1.txt col2.txt", shell=True, universal_newlines=True)
+print(res)
