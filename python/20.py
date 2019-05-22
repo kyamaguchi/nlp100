0272817300004e14af1b75d23708fa80bd89329e
kyamaguchi <kzh.yap@gmail.com>
2019-05-22 12:54:19 +0900

    Redo 21 with the proper data source

--- python/20.py
+++ python/20.py
@@ -3,24 +3,35 @@
 print("20. JSONデータの読み込み")
 print("Wikipedia記事のJSONファイルを読み込み，「イギリス」に関する記事本文を表示せよ．問題21-29では，ここで抽出した記事本文に対して実行せよ．")
 
-import os
-import urllib.parse
-import requests
+import gzip
 import json
-import pprint
+fname = 'jawiki-country.json.gz'
 
-title = urllib.parse.quote('イギリス')
-url = "https://ja.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&titles=%s&format=json" %(title)
+with gzip.open(fname, "rt") as f:
+    for line in f:
+        data_json = json.loads(line)
+        # print(data_json['title'])
+        if data_json['title'] == 'イギリス':
+            print(data_json['text'])
 
-filename = 'United_Kingdom_raw.json'
+# import os
+# import urllib.parse
+# import requests
+# import json
+# import pprint
 
-if not os.path.exists(filename):
-    res = requests.get(url)
-    res.raise_for_status()
-    with open(filename, mode='wb') as outfile:
-        print(res.content)
-        outfile.write(res.content)
+# title = urllib.parse.quote('イギリス')
+# url = "https://ja.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&titles=%s&format=json" %(title)
 
-with open(filename, 'r') as f:
-    json = json.load(f)
-    pprint.pprint(json)
+# filename = 'United_Kingdom_raw.json'
+
+# if not os.path.exists(filename):
+#     res = requests.get(url)
+#     res.raise_for_status()
+#     with open(filename, mode='wb') as outfile:
+#         print(res.content)
+#         outfile.write(res.content)
+
+# with open(filename, 'r') as f:
+#     json = json.load(f)
+#     pprint.pprint(json)
