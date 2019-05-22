4ed5aee76d4bbbf6778faa1dc0bee0eadcb03042
kyamaguchi <kzh.yap@gmail.com>
2019-05-22 11:55:03 +0900

    Solve NLP 19

--- python/19.py
+++ python/19.py
@@ -2,3 +2,22 @@
 
 print("19. 各行の1コラム目の文字列の出現頻度を求め，出現頻度の高い順に並べる")
 print("各行の1列目の文字列の出現頻度を求め，その高い順に並べて表示せよ．確認にはcut, uniq, sortコマンドを用いよ．")
+
+lines = open('hightemp.txt').readlines()
+words = [x.split('\t')[0] for x in lines]
+
+counts = {}
+for x in words:
+    if x in counts.keys():
+        counts[x] += 1
+    else:
+        counts[x] = 1
+
+for k, v in sorted(counts.items(), key=lambda x: [x[1], x[0]], reverse=True):
+    print(v, k)
+
+print()
+
+import subprocess
+res = subprocess.check_output('cut -f 1 hightemp.txt | sort | uniq -c | sort -r -n -k 1', shell=True, universal_newlines=True)
+print(res)
