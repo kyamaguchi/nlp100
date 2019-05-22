b884d8d313be828f60051f6d81f2badacb0fee48
kyamaguchi <kzh.yap@gmail.com>
2019-05-22 13:02:58 +0900

    Redo 21 with the proper data source

--- python/21.py
+++ python/21.py
@@ -7,13 +7,12 @@ import json
 import re
 import pprint
 
-filename = 'United_Kingdom_raw.json'
+filename = 'United_Kingdom.txt'
 
 with open(filename, 'r') as f:
-    json = json.load(f)
-    lines = list(json['query']['pages'].values())[0]['revisions'][0]['*'].split('\n')
+    lines = f.readlines()
     # pprint.pprint(lines)
     # print(len(lines))
 
     categories = [l for l in lines if re.search('\[\[Category', l)]
-    print(categories)
+    print(''.join(categories))
