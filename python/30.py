915780054737584194fa417391fff0aeff7e7ece
kyamaguchi <kzh.yap@gmail.com>
2019-05-25 12:30:34 +0900

    Solve NLP 30

--- python/30.py
+++ python/30.py
@@ -2,3 +2,26 @@
 
 print("30. 形態素解析結果の読み込み")
 print("形態素解析結果（neko.txt.mecab）を読み込むプログラムを実装せよ．ただし，各形態素は表層形（surface），基本形（base），品詞（pos），品詞細分類1（pos1）をキーとするマッピング型に格納し，1文を形態素（マッピング型）のリストとして表現せよ．第4章の残りの問題では，ここで作ったプログラムを活用せよ．")
+
+groups = []
+group = []
+
+for line in open('neko.txt.mecab').readlines():
+    if line.startswith('EOS'):
+        if len(group) > 0:
+            groups.append(group)
+            group = []
+        continue
+
+    elm = {}
+    parts = line.split('\t')
+    elm['surface'] = parts[0]
+    parts2 = parts[1].split(',')
+    elm['pos'] = parts2[0]
+    elm['pos1'] = parts2[2]
+    if len(parts2) == 9:
+        elm['base'] = parts2[6]
+    group.append(elm)
+
+for g in groups:
+    print(g)
