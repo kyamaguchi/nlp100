76713b437cc7431387f8bbf270f7d1ce9c1be5cd
kyamaguchi <kzh.yap@gmail.com>
2019-05-29 15:05:50 +0900

    Solve NLP 38

--- python/38.py
+++ python/38.py
@@ -2,3 +2,15 @@
 
 print("38. ヒストグラム")
 print("単語の出現頻度のヒストグラム（横軸に出現頻度，縦軸に出現頻度をとる単語の種類数を棒グラフで表したもの）を描け．")
+
+import common
+groups = common.extract_groups_from_mecab('neko.txt.mecab')
+counts = common.word_counts(groups)
+
+freq = list(counts.values())
+freq.sort(reverse=True)
+
+import matplotlib.pyplot as plt
+
+plt.hist(freq, bins=50, range=(0, 50))
+plt.show()
